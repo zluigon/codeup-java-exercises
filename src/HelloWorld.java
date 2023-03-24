72f11ba4e6d74071b7a8ed5e1a2c57cb9b744324
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-24 12:43:20 -0400

    syntax, types, and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,50 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(("Hello, World!"));
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "class"
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+        byte max = 127;
+        System.out.println(++max);
+
     }
 }
