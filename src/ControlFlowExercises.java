eea04210747d19347e20b423f141ec123b3de4e3
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-27 11:20:38 -0400

    exercise b complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,40 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
+//        int i = 5;
+//
+//        while ( i <= 15) {
+//            System.out.println("i is: "+ i);
+//            i++;
+//        }
 
-        while ( i <= 15) {
-            System.out.println("i is: "+ i);
-            i++;
+//        int i = 0;
+//        do{
+//            System.out.println(i);
+//            i += 2;
+//        }while (i <= 100);
+
+        for (int i = 0; i <= 100; i+=2 ){
+            System.out.println(i);
+        }
+
+//        int i = 100;
+//        do{
+//            System.out.println(i);
+//            i -= 5;
+//        }while (i >= -10);
+
+        for(int i = 0; i >= -10; i -= 5){
+            System.out.println(i);
+        }
+
+//        long i = 2; // why long vs int?
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+
+        for (long i = 2; i < 1000000; i *= i){
+            System.out.println(i);
         }
     }
 }
