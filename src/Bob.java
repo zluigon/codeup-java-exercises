7fce705f362c3612db88a6b84d565bf6edd907f1
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-27 15:42:10 -0400

    allow for repeated converstaion with Bob w/o having to rerun

--- src/Bob.java
+++ src/Bob.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("What would you like to say to Bob?");
-        String userResponse = scanner.nextLine();
+        String userResponse;
 
-        if(userResponse.endsWith("?")){
-            System.out.println("Bob: Sure");
-        } else if (userResponse.endsWith("!")){
-            System.out.println("Bob: Whoa, chill out!");
-        } else if (userResponse.isEmpty()){
-            System.out.println("Fine. Be that way!");
-        } else {
-            System.out.println("Bob: Whatever.");
-        }
+        do {
+            System.out.println("If you'd like to stop talking to Bob, say \"Goodbye\".");
+            userResponse = scanner.nextLine();
+            if (userResponse.endsWith("?")) {
+                System.out.println("Bob: Sure");
+            } else if (userResponse.endsWith("!")) {
+                System.out.println("Bob: Whoa, chill out!");
+            } else if (userResponse.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+            } else if (userResponse.equalsIgnoreCase("goodbye")) {
+                System.out.println("Bob: Later.");
+            } else {
+                System.out.println("Bob: Whatever.");
+            }
+        } while (!userResponse.equalsIgnoreCase("goodbye"));
     }
 }
