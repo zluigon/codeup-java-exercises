51dd30ff9c53f939bdd8f9158776d566667693a0
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-27 11:42:44 -0400

    exercise 2 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+//        exercise 1a:
+
 //        int i = 5;
-//
 //        while ( i <= 15) {
 //            System.out.println("i is: "+ i);
 //            i++;
 //        }
+
+//        exercise 1b:
 
 //        int i = 0;
 //        do{
@@ -13,19 +17,11 @@ public class ControlFlowExercises {
 //            i += 2;
 //        }while (i <= 100);
 
-        for (int i = 0; i <= 100; i+=2 ){
-            System.out.println(i);
-        }
-
 //        int i = 100;
 //        do{
 //            System.out.println(i);
 //            i -= 5;
 //        }while (i >= -10);
-
-        for(int i = 0; i >= -10; i -= 5){
-            System.out.println(i);
-        }
 
 //        long i = 2; // why long vs int?
 //        do {
@@ -33,8 +29,32 @@ public class ControlFlowExercises {
 //            i *= i;
 //        } while (i < 1000000);
 
-        for (long i = 2; i < 1000000; i *= i){
-            System.out.println(i);
+//        exercise 1c:
+//
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 0; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+//
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+//        exercise 2:
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz: " + i);
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz: " + i);
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz: " + i);
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
