b122d69c25044896cbeb1daba804d850c6dc1382
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-28 12:44:53 -0400

    refactored exercise 3 to be recursive using the  getInt method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,16 +1,6 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-    public static void main(String[] args) {
-//        System.out.println("Addition:" + addition(1, 2));
-//        System.out.println("Subtraction:" + subtraction(5, 3));
-//        System.out.println("Multiplication: " + multiplication(6, 6));
-//        System.out.println("Division: " + division(100, 5));
-//        System.out.println("Modulus: " + modulus(13, 3));
-        int number = getInteger(1, 10);
-        factorial(number);
-    }
-
     public static int addition(int num1, int num2) {
         return num1 + num2;
     }
@@ -31,6 +21,16 @@ public class MethodsExercises {
         return num1 % num2;
     }
 
+    public static void main(String[] args) {
+//        System.out.println("Addition:" + addition(1, 2));
+//        System.out.println("Subtraction:" + subtraction(5, 3));
+//        System.out.println("Multiplication: " + multiplication(6, 6));
+//        System.out.println("Division: " + division(100, 5));
+//        System.out.println("Modulus: " + modulus(13, 3));
+       int num =  getInteger(1, 10);
+        factorial(num);
+    }
+
     public static int getInteger(int min, int max) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Enter a number between %d and %d: ", min, max);
@@ -39,7 +39,7 @@ public class MethodsExercises {
             System.out.printf("I'm sorry, %d is not within range.%n", userInput);
             return getInteger(min, max);
         }
-        System.out.printf("Nice! %d is within the range.", userInput);
+        System.out.printf("Nice! %d is within the range.%n", userInput);
         return userInput;
     }
 
@@ -60,7 +60,8 @@ public class MethodsExercises {
         boolean userConfirm = scanner.next().equalsIgnoreCase("y");
 
         if (userConfirm) {
-            factorial(number);
+            int num =  getInteger(1, 10);
+            factorial(num);
         } else {
             System.out.println("End.");
         }
