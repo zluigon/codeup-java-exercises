aa9d2427a47aa14a51e2c5aa618bcc2e90ccff4b
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-29 21:51:42 -0400

    array basics complete

---
+++ src/ArraysExercises.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static Person[] addPerson (Person[] people, Person person){
+        people = Arrays.copyOf(people, people.length + 1);
+        people[people.length - 1] = person;
+        return people;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        Person[] people = new Person[3];
+
+        people[0] = new Person("Luis");
+        people[1] = new Person("Sean");
+        people[2] = new Person("Kaleb");
+
+        Person kenneth = new Person("Kenneth");
+        people = addPerson(people, kenneth);
+
+        for (Person peoples : people){
+            System.out.println(peoples.getName());
+        }
+
+    }
+}
