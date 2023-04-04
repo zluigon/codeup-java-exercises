0490b4b8906a28f75d3243883c6bb24a480a80e3
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-04-04 10:45:25 -0400

    added setters to square and rectangle

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -17,12 +17,12 @@ public class Rectangle extends Quadrilateral implements Measurable {
 
     @Override
     public void setLength(double length) {
-
+        this.length = length;
     }
 
     @Override
     public void setWidth(double width) {
-
+        this.width = width;
     }
 //
 //    protected double length;
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -17,12 +17,14 @@ public class Square extends Quadrilateral {
 
     @Override
     public void setLength(double length) {
-
+        this.length = length;
+        this.width = length;
     }
 
     @Override
     public void setWidth(double width) {
-
+        this.width = width;
+        this.length = width;
     }
 //
 //    public Square(double side) {
