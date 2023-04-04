6872c54af7f5ae9020755f3a2be9cdc792c3fa88
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-04-04 12:20:54 -0400

    added side property to square and updated methods

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,30 +1,32 @@
 package shapes;
 
 public class Square extends Quadrilateral {
+
+    protected double side;
+
     public Square(double side) {
         super(side, side);
+        this.side = side;
     }
 
     @Override
     public double getPerimeter() {
-        return 4 * length;
+        return 4 * side;
     }
 
     @Override
     public double getArea() {
-        return Math.pow(length, 2);
+        return Math.pow(side, 2);
     }
 
     @Override
     public void setLength(double length) {
-        this.length = length;
-        this.width = length;
+
     }
 
     @Override
     public void setWidth(double width) {
-        this.width = width;
-        this.length = width;
+
     }
 //
 //    public Square(double side) {
